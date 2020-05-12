38917542f044052cd4a69fbd19dc84f18242d609
BCMarks <bmarks@student.wethinkcode.co.za>
2020-05-12 15:46:18 +0200

    Delay added between downloads. Now a class

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ Scraping country flags from https://www.countryflags.com/en/ using Python and se
 The flag images should be saved in your default download location.
 
 # Setup
-Ensure that selenium is installed.
 In the terminal run:
-`pip instal selenium`
+`pip install -r requirements.txt`
 
 # Run
 `py flagScraper.py` or `python flagScraper.py`
 If asked, allow access to the network.
+
+# Outcome
+Expected 196 flags to be downloaded
--- flagScraper.py
+++ flagScraper.py
@@ -1,4 +1,4 @@
-import urllib.request
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,53 +6,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 
-# r"string" SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes in position 2-3: truncated \UXXXXXXXX escape
-path = r"C:\Users\bbdnet2210\Downloads\chromedriver_win32\chromedriver.exe"
-baseURL = "https://www.countryflags.com/en/"
+class flagScraper:
+    def __init__(self):
+        path = "chromedriver.exe"
+        baseURL = "https://www.countryflags.com/en/"
 
-option = webdriver.ChromeOptions()
-option.add_argument("--incognito")
+        option = webdriver.ChromeOptions()
+        option.add_argument("--incognito")
 
-browser = webdriver.Chrome(executable_path=path, options=option)
+        browser = webdriver.Chrome(path, options=option)
 
-browser.get(baseURL)
+        browser.get(baseURL)
 
-timeout = 120
-try:
-    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='img-responsive']")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    browser.quit()
+        timeout = 120
+        try:
+            WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='img-responsive']")))
+        except TimeoutException:
+            print("Timed out waiting for page to load")
+            browser.quit()
 
-flagLinks = browser.find_elements_by_xpath("//a[@class='clearfix']")
-flagThumbnails = browser.find_elements_by_xpath("//img[@class='img-responsive']")
+        flagLinks = browser.find_elements_by_xpath("//a[@class='clearfix']")\
 
-urls = []
-for link in flagLinks:
-    link_href = link.get_property("href")
-    urls.append(link_href)
+        urls = []
+        for link in flagLinks:
+            link_href = link.get_property("href")
+            urls.append(link_href)
 
-for link in urls:
-    browser.get(link)
-    try:
-        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='btn btn-primary btn-custom-image']")))
-    except TimeoutException:
-        print("Timed out waiting for page2 to load")
-        browser.quit()
+        for link in urls:
+            print("Attempting to download "+link.split("/")[-1].split(".")[0])
+            browser.get(link)
+            try:
+                WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='btn btn-primary btn-custom-image']")))
+            except TimeoutException:
+                print("Timed out waiting for page2 to load")
+                browser.quit()
 
-    imageButton = browser.find_element_by_xpath("//a[@class='btn btn-primary btn-custom-image']")
-    imageButton.click()
+            imageButton = browser.find_element_by_xpath("//a[@class='btn btn-primary btn-custom-image']")
+            imageButton.click()
 
-    try:
-        WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='btn btn-default']")))
-    except TimeoutException:
-        print("Timed out waiting for page3 to load")
-        browser.quit()
+            try:
+                WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='btn btn-default']")))
+            except TimeoutException:
+                print("Timed out waiting for page3 to load")
+                browser.quit()
 
-    buttons = browser.find_elements_by_xpath("//a[@class='btn btn-default']")
+            buttons = browser.find_elements_by_xpath("//a[@class='btn btn-default']")
 
-    for button in buttons:
-        if button.get_property("href").split("/")[-1] == "flag-png-medium.png":
-            print("Downloading "+button.get_property("href").split("/")[-2]+"...")
-            button.click()
-            break
+            for button in buttons:
+                if button.get_property("href").split("/")[-1] == "flag-png-medium.png":
+                    print("Downloading "+button.get_property("href").split("/")[-2]+"...")
+                    button.click()
+                    break
+            sleep(10)
+
+flagScraper()
